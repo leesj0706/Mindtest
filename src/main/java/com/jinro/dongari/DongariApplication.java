1e780386d878d54f8439c0a4605800e11b3deb12
DESKTOP-E7S6P6O\u <leesj9627@naver.com>
2025-11-30 16:43:16 +0900

    Initial commit

---
+++ src/main/java/com/jinro/dongari/DongariApplication.java
@@ -0,0 +1,13 @@
+package com.jinro.dongari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongariApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jinro/dongari/DongariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinro.dongari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
